34d09b4f4f3db2013ab0f837bee95b12ecd1291e
Dalton Eggleston <egglestondalton@yahoo.com>
2022-07-02 23:36:49 -0700

    home page done

--- commerce_project/commerce_app/views.py
+++ commerce_project/commerce_app/views.py
@@ -1,3 +1,9 @@
+import re
 from django.shortcuts import render
+from urllib import response
+import requests as HTTP_Client
 
 # Create your views here.
+def index(request):
+    
+    return render(request, 'index.html')
